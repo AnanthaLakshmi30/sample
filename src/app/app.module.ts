51a81ba5a37c96934b809bdd4b6b656763b59581
AnanthaLakshmi30 <108463092+AnanthaLakshmi30@users.noreply.github.com>
2024-04-29 12:11:11 +0530

    intial command

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TestModule } from './test/test.module';
+import { Test1Component } from './test/test1/test1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TestModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
